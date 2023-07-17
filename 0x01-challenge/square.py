22c610bfc8a3ff3065c9d44d28e57c3679b50ef0
Koyejo <adinlewakoyejo@yahoo.com>
2023-07-17 06:54:46 -0700

    Made pycodestyle corrections

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Create a class - 'Square'"""
 
-class square():
-    
+
+class Square():
+    """Implement a class - 'Square'"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Initialise instances of the class"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +17,17 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String representation of the class"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
